ac0f09edd7e830f81d21ea0e41f2a7927fab953a
Mazz <maazbinnaseer@gmail.com>
2023-08-02 22:04:15 +0000

    Copied the contents of ex00 to ex01

--- Module05/ex01/Bureaucrat.hpp
+++ Module05/ex01/Bureaucrat.hpp
@@ -1,13 +1,32 @@
 #ifndef BUREAUCRAT_CPP
 #define BUREAUCRAT_CPP
 #include <iostream>
-#include <stdbool.h>
 
 #define SUCCESS 0
 #define FAILURE 1
 
 #define RED "\033[1;31m"
 #define RESET "\033[0m"
+
+class GradeHigh: public std::exception
+{
+    public:
+        virtual const char* what() const throw()
+        {
+            return  "Burecrate::GradeTooHigh";
+        }
+
+};
+
+class GradeLow: public std::exception
+{
+    public:
+        virtual const char* what() const throw()
+        {
+            return  "Burecrate::GradeTooLow";
+        }
+
+};
 class Bureaucrate
 {
     private:
@@ -22,9 +41,6 @@ class Bureaucrate
         int decrementGrade(int decrement_value);
         ~Bureaucrate();
 };
-
-
-
 
 
 #endif
